056940f6273603a8566db1cb6f0edf24362ef009
DenisPprg <denis.pprg@gmail.com>
2021-07-20 14:56:46 +0300

    Code refactor

--- sources/src/Controller/LuckyController.php
+++ sources/src/Controller/LuckyController.php
@@ -5,11 +5,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
-    /**
-     *Example file/actions
-     *
-     * @Route("/lucky/test", name="lucky_test")
-     */
     public function test()
     {
        echo 'Hello Denis';
