69ff187507bd4ee135c838a5f3ce2c1d7442ac2d
DenisPprg <denis.pprg@gmail.com>
2021-08-12 18:55:17 +0300

    Work with DB unv_files

---
+++ sources/src/Entity/User.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @var $id
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var $login
+     * @ORM\Column(type="string", length=255)
+     */
+    private $login;
+
+    /**
+     * @var $password
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @return integer
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param string $login
+     * @return $this
+     */
+    public function setLogin($login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return $this
+     */
+    public function setPassword($password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
